ecf29fe3570dfcf51ee3bb6e12b344e4f5e3f52c
AnthonyAniobi <anthonyaniobi198@gmail.com>
2022-08-22 15:13:11 +0100

    solved tower of hanoi problem

---
+++ topics/recursion/towerOfHanoi.py
@@ -0,0 +1,17 @@
+def towerOfHanoi(num:int)->None:
+    '''tower of hanoi solution for num tiles'''
+    moves(num, source='source', auxilliary='auxilliary', destination='destination')
+    print('solved')
+
+def moves(num:int, source:str, auxilliary:str, destination:str):
+    '''moves for the tower of hanoi problem'''
+    if num == 1:
+        print(f'move tile {num} from {source} to {destination}')
+    else:
+        moves(num-1, source, destination, auxilliary)
+        print(f'move tile {num} from {source} to {destination}')
+        moves(num-1, auxilliary, source, destination)
+
+if __name__ == '__main__':
+    number:int = 3
+    towerOfHanoi(number)
