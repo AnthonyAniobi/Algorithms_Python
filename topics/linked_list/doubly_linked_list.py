eb1311463ae0b8455449e95e7daa03d72a609715
AnthonyAniobi <anthonyaniobi198@gmail.com>
2022-08-27 16:28:01 +0100

    worked on doubly linked list

--- topics/linked_list/doubly_linked_list.py
+++ topics/linked_list/doubly_linked_list.py
@@ -44,7 +44,7 @@ class DoublyLinkedList:
         self.tail = None
         self.length = 0
     
-    def append(self, data):
+    def insertAtEnd(self, data):
         newNode = Node()
         newNode.setData(data)
         if self.head == None:
@@ -61,14 +61,81 @@ class DoublyLinkedList:
             self.tail = newNode
             self.length +=1
 
+    def insertAtStart(self, data):
+        newNode = Node()
+        newNode.setData(data)
+        if not self.length:
+            self.head = newNode
+            self.tail = newNode
+            self.length = 1
+        else:
+            newNode.setNext(self.head)
+            self.head.setPrev(newNode)
+            if self.length == 1:
+                self.tail = self.head
+                self.tail.setPrev(self.head)
+            self.head = newNode
+            self.length += 1
+    
+    def insertAtPosition(self, position, data):
+        if position > self.length:
+            raise IndexError()
+        elif position-1 == 0:
+            self.insertAtStart(data)
+        elif position == self.length:
+            self.insertAtEnd(data)
+        else:
+            newNode = Node()
+            newNode.setData(data)
+            currentNode = self.head
+            for i in range(position-2):
+                currentNode = currentNode.getNext()
+            newNode.setPrev(currentNode)
+            newNode.setNext(currentNode.getNext())
+            currentNode.setNext(newNode)
+            nextNode = currentNode.getNext()
+            nextNode.setPrev(newNode)
+            self.length +=1
+
+    def deleteAtBeginning(self):
+        newHead = self.head.getNext()
+        newHead.setPrev(None)
+        self.head = newHead
+        self.length -= 1
+
+    def deleteAtEnd(self):
+        newTail = self.tail.getPrev()
+        prevPrevTail = newTail.getPrev()
+        newTail.setNext(None)
+        newTail.setPrev(prevPrevTail)
+        self.length -= 1
+
+    def deleteValue(self, data):
+        currentNode = self.head
+        if currentNode.data == data:
+            self.deleteAtBeginning()
+        while currentNode.next:
+            if currentNode.data == data:
+                prevNode = currentNode.getPrev()
+                nextNode = currentNode.getNext()
+                prevNode.setNext(nextNode)
+                nextNode.setPrev(prevNode)
+                currentNode = None
+                self.length -= 1
+                return
+            currentNode = currentNode.getNext()
+        raise Exception('data not found')
+
+
 if __name__ == '__main__':
     linked_list = DoublyLinkedList()
-    linked_list.append(0)
-    linked_list.append(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.append(4)
-    linked_list.append(5)
+    linked_list.insertAtStart(0)
+    linked_list.insertAtStart(1)
+    linked_list.insertAtStart(2)
+    linked_list.insertAtStart(3)
+    linked_list.insertAtStart(4)
+    linked_list.insertAtStart(5)
+    linked_list.deleteValue(5)
 
     print(linked_list)
     print(linked_list.length)
