f1300f908450ebb5479765b26aa1365ede6dec21
Anthony <AnthonyAniobi>
2022-10-14 15:58:18 +0100

    solved!

--- challenges/kickstart2022A1.py
+++ challenges/kickstart2022A1.py
@@ -1,31 +1,23 @@
 def solve(case_index, required:str, entry:str):
-    modifiable_count = 0
-    entry_index = 0
-    for index in range(len(required)):
-        if(entry[entry_index]==required[index]):
-            entry_index+=1
-        else:
-            modifiable_count += 1
-    if entry_index <= len(required)-1:
-        return f'Case #{case_index}: {modifiable_count}'
+    required_index = 0
+    for index in range(len(entry)):
+        if required_index>=len(required):
+            break
+        if(required[required_index]==entry[index]):
+            required_index+=1
+    if required_index == len(required):
+        return f'Case #{case_index}: {len(entry)-len(required)}'
     else:
         return f'Case #{case_index}: IMPOSSIBLE'
+        
 
-# input_value = input().split('\n')
-input_value = '''
-2
-aaaa
-aaaaa
-bbbbb
-bbbbb
-'''
+test_cases = int(input())
+input_value = list()
 
-input_value = input_value.strip()
-input_value = input_value.split('\n')
-test_cases =  int(input_value[0])
-typing_test_cases = (len(input_value)-1)//2
-
-for val in range(typing_test_cases):
+for i in range(test_cases*2):
+    input_value.append(input())
+    
+for val in range(test_cases):
     required = input_value[val*2]
     entry = input_value[(val*2)+1]
     
